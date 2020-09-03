fefc9636bed100c6749a52eb8e51288034106dc7
ocsarpo <ocsarpo22@gmail.com>
2020-09-04 01:43:35 +0900

    Signin

--- recordstore-front/src/router/index.js
+++ recordstore-front/src/router/index.js
@@ -4,8 +4,12 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
+      path: '/',
+      name: 'Signin',
+      component: () => import('../components/Signin.vue')
     }
   ]
 })
